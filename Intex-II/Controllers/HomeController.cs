d34d11e1bf376f9dc4b4b049fcb0a96fb947c4d1
cdawg1213 <103866953+cdawg1213@users.noreply.github.com>
2024-04-08 08:16:38 -0600

    Add project files.

---
+++ Intex-II/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Intex_II.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Intex_II.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
