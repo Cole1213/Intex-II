8cd16c2742d21a82bf8c6f29ab734725ac3a05d3
cdawg1213 <103866953+cdawg1213@users.noreply.github.com>
2024-04-08 11:27:47 -0600

    Models and empty views created

---
+++ Intex-II/Models/LineItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Intex_II.Models
+{
+    public class LineItem
+    {
+        [ForeignKey("Transaction_Id")]
+        public int Transaction_Id { get; set; }
+
+        [ForeignKey("Product_Id")]
+        public int Product_Id { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int? Rating { get; set; }
+    }
+}
---
+++ Intex-II/Models/Order.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Intex_II.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Transaction_Id { get; set; }
+
+        [ForeignKey("Customer_Id")]
+        public int Customer_Id { get; set; }
+
+        public DateOnly Transaction_Date { get; set; }
+
+        public string? Transaction_Day_Of_Week { get; set; }
+
+        public int? Transaction_Time { get; set; }
+
+        public string Entry_Mode { get; set; }
+
+        public int Amount {  get; set; }
+
+        public string Transaction_Type { get; set; }
+
+        public string Transaction_Country { get; set; }
+
+        public string Transaction_Shipping_Address { get; set; }
+
+        public string Transaction_Bank { get; set; }
+
+        public string Transaction_Type_Of_Card { get; set; }
+
+        public bool? Fraud { get; set; }
+    }
+}
