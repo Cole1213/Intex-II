73d875f4f1f4ec1c8fef27bda60b608c1d74126e
cdawg1213 <103866953+cdawg1213@users.noreply.github.com>
2024-04-09 10:06:58 -0600

    Made changes to the categories filters, added a Cart model, and edited Customer Model

---
+++ Intex-II/Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Intex_II.Models;
+
+public partial class Cart
+{
+    public int CustomerId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public int ItemQuantity { get; set; }
+
+    public int TotalPrice { get; set; }
+}
